93475cb1f4bc391fffaaa8dc59970b0d13d3182c
Sangho Kim <marchbreeze03@gmail.com>
2024-05-14 02:16:06 +0900

    [#21] 컴포저블 계층 설정

---
+++ app/src/main/java/march/breeze/compose/CompLocalDemoActivity.kt
@@ -0,0 +1,83 @@
+package march.breeze.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import march.breeze.compose.ui.theme.ComposeStudyTheme
+
+class CompLocalDemoActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeStudyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Composable1()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Composable1() {
+    Column {
+        Composable2()
+        Composable3()
+    }
+}
+
+@Composable
+fun Composable2() {
+    Composable4()
+}
+
+@Composable
+fun Composable3() {
+    Composable5()
+}
+
+@Composable
+fun Composable4() {
+    Composable6()
+}
+
+@Composable
+fun Composable5() {
+    Composable7()
+    Composable8()
+}
+
+@Composable
+fun Composable6() {
+    Text(text = "Composable 6")
+}
+
+@Composable
+fun Composable7() {
+
+}
+
+@Composable
+fun Composable8() {
+    Text(text = "Composable 8")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeStudyTheme {
+        Composable1()
+    }
+}
