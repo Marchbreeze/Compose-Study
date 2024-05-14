f237971730a5238f55b52a4dae4aefd7e9793ee0
Sangho Kim <marchbreeze03@gmail.com>
2024-05-14 16:37:18 +0900

    [#23] 체크박스 컴포저블 추가

--- app/src/main/java/march/breeze/compose/SlotApiDemoActivity.kt
+++ app/src/main/java/march/breeze/compose/SlotApiDemoActivity.kt
@@ -5,9 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import march.breeze.compose.ui.theme.ComposeStudyTheme
 
 class SlotApiDemoActivity : ComponentActivity() {
@@ -33,7 +41,7 @@ class SlotApiDemoActivity : ComponentActivity() {
     }
 }
 
-// 컴포저블 생성
+// 상태와 이벤트 핸들러를 보유하는 컴포저블 생성
 @Composable
 fun MainScreen() {
     var linearSelected by remember { mutableStateOf(true) }
@@ -61,4 +69,35 @@ fun ScreenContent(
     ) {
 
     }
+}
+
+// 체크박스 컴포저블
+@Composable
+fun CheckBoxes(
+    linearSelected: Boolean,
+    imageSelected: Boolean,
+    onTitleClick: (Boolean) -> Unit,
+    onLinearClick: (Boolean) -> Unit,
+) {
+    Row(
+        Modifier.padding(20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(checked = imageSelected, onCheckedChange = onTitleClick)
+        Text(text = "Image Title")
+        Spacer(modifier = Modifier.width(20.dp))
+        Checkbox(checked = linearSelected, onCheckedChange = onLinearClick)
+        Text(text = "Linear Progress")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DemoPreview() {
+    CheckBoxes(
+        linearSelected = true,
+        imageSelected = true,
+        onTitleClick = {},
+        onLinearClick = {},
+    )
 }
