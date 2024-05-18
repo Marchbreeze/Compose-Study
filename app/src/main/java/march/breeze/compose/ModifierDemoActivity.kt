72a05fe2a6a894e3e665f77c0fae2dde197f4c26
Sangho Kim <marchbreeze03@gmail.com>
2024-05-18 19:40:58 +0900

    [#24] 모디파이어 텍스트 적용

--- app/src/main/java/march/breeze/compose/ModifierDemoActivity.kt
+++ app/src/main/java/march/breeze/compose/ModifierDemoActivity.kt
@@ -3,14 +3,18 @@ package march.breeze.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import march.breeze.compose.ui.theme.ComposeStudyTheme
 
@@ -33,8 +37,12 @@ class ModifierDemoActivity : ComponentActivity() {
 
 @Composable
 fun DemoScreen2() {
+    val modifier = Modifier
+        .padding(all = 10.dp)
+        .border(width = 2.dp, color = Color.Blue)
     Text(
         text = "Hello",
+        modifier = modifier,
         fontSize = 40.sp,
         fontWeight = FontWeight.Bold
     )
