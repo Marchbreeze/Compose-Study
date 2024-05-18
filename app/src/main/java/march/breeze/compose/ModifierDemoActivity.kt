8d3201fcb58b5afed5fe87c22d2907ff28b851a5
Sangho Kim <marchbreeze03@gmail.com>
2024-05-18 20:19:42 +0900

    [#24] 모디파이어 지원 추가

--- app/src/main/java/march/breeze/compose/ModifierDemoActivity.kt
+++ app/src/main/java/march/breeze/compose/ModifierDemoActivity.kt
@@ -3,15 +3,25 @@ package march.breeze.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,13 +48,36 @@ class ModifierDemoActivity : ComponentActivity() {
 @Composable
 fun DemoScreen2() {
     val modifier = Modifier
-        .padding(all = 10.dp)
         .border(width = 2.dp, color = Color.Blue)
-    Text(
-        text = "Hello",
-        modifier = modifier,
-        fontSize = 40.sp,
-        fontWeight = FontWeight.Bold
+        .padding(all = 10.dp)
+    Column(
+        Modifier.padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Hello",
+            modifier = modifier,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        CustomImage(
+                image = R.drawable.vacation,
+                Modifier
+                    .padding(16.dp)
+                    .width(270.dp)
+                    .clip(shape = RoundedCornerShape(30.dp))
+            )
+    }
+}
+
+@Composable
+fun CustomImage(image: Int, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = image),
+        contentDescription = null,
+        modifier
     )
 }
 
