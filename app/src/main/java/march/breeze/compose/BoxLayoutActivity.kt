dec39966d1696b4a5313c0bd0a9833f077b61b18
Sangho Kim <marchbreeze03@gmail.com>
2024-05-20 23:58:20 +0900

    [#26] clip 모디파이어 활용

--- app/src/main/java/march/breeze/compose/BoxLayoutActivity.kt
+++ app/src/main/java/march/breeze/compose/BoxLayoutActivity.kt
@@ -3,17 +3,20 @@ package march.breeze.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -58,7 +61,12 @@ fun TextFontCell(text: String, modifier: Modifier = Modifier, fontSize: Int = 15
 
 @Composable
 fun MainScreen6() {
-    Box (modifier = Modifier.size(90.dp, 90.dp)){
+    Box(
+        modifier = Modifier
+            .size(90.dp, 90.dp)
+            .clip(RoundedCornerShape(10.dp))
+            .background(Color.Gray)
+    ) {
         Text(text = "1", Modifier.align(Alignment.TopStart))
         Text(text = "2", Modifier.align(Alignment.Center))
         Text(text = "3", Modifier.align(Alignment.BottomEnd))
