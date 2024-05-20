be15e6d199c3f5aa82d86aefce316a27a44498aa
Sangho Kim <marchbreeze03@gmail.com>
2024-05-21 00:55:58 +0900

    [#27] 정렬 선 다루기

--- app/src/main/java/march/breeze/compose/LayoutModifierActivity.kt
+++ app/src/main/java/march/breeze/compose/LayoutModifierActivity.kt
@@ -5,17 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import march.breeze.compose.ui.theme.ComposeStudyTheme
+import kotlin.math.roundToInt
 
 class LayoutModifierActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +31,22 @@ class LayoutModifierActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainScreen6()
+                    MainScreen7()
                 }
             }
         }
+    }
+}
+
+fun Modifier.exampleLayout(
+    fraction: Float
+) = layout { measurable, constraints ->
+    val placeable = measurable.measure(constraints)
+
+    val x = -(placeable.width * fraction).roundToInt()
+
+    layout(placeable.width, placeable.height) {
+        placeable.placeRelative(x = x, y = 0)
     }
 }
 
@@ -46,8 +62,24 @@ fun ColorBox(modifier: Modifier) {
 
 @Composable
 fun MainScreen7() {
-    Box(modifier = Modifier.size(120.dp, 80.dp)) {
-        ColorBox(modifier = Modifier.background(Color.Blue))
+    Box(contentAlignment = Alignment.Center, modifier = Modifier.size(120.dp, 80.dp)) {
+        Column {
+            ColorBox(
+                modifier = Modifier
+                    .exampleLayout(0f)
+                    .background(Color.Blue)
+            )
+            ColorBox(
+                modifier = Modifier
+                    .exampleLayout(0.25f)
+                    .background(Color.Gray)
+            )
+            ColorBox(
+                modifier = Modifier
+                    .exampleLayout(0.5f)
+                    .background(Color.Green)
+            )
+        }
     }
 }
 
@@ -55,6 +87,6 @@ fun MainScreen7() {
 @Composable
 fun GreetingPreview5() {
     ComposeStudyTheme {
-        MainScreen6()
+        MainScreen7()
     }
 }
