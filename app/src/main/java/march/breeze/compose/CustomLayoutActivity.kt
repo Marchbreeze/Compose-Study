76bd48fffee4c13c102e9e21958501d6b35a8e9d
Sangho Kim <marchbreeze03@gmail.com>
2024-05-21 15:41:41 +0900

    [#28] CustomLayout 으로 Cascade 구현

--- app/src/main/java/march/breeze/compose/CustomLayoutActivity.kt
+++ app/src/main/java/march/breeze/compose/CustomLayoutActivity.kt
@@ -3,13 +3,18 @@ package march.breeze.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import march.breeze.compose.ui.theme.ComposeStudyTheme
 
 class CustomLayoutActivity : ComponentActivity() {
@@ -30,14 +35,47 @@ class CustomLayoutActivity : ComponentActivity() {
 }
 
 @Composable
+fun CascadeLayout(
+    spacing: Int = 0,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        var indent = 0
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            val placeables = measurables.map { measurable ->
+                measurable.measure(constraints)
+            }
+            var yCoord = 0
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = indent, y = yCoord)
+                indent += placeable.width + spacing
+                yCoord += placeable.height + spacing
+            }
+        }
+    }
+}
+
+@Composable
 fun MainScreen8() {
-    
+    Box {
+        CascadeLayout(spacing = 20) {
+            Box(modifier = Modifier.size(60.dp).background(Color.Blue))
+            Box(modifier = Modifier.size(80.dp, 40.dp).background(Color.Green))
+            Box(modifier = Modifier.size(70.dp, 100.dp).background(Color.Gray))
+            Box(modifier = Modifier.size(50.dp).background(Color.Cyan))
+            Box(modifier = Modifier.size(20.dp).background(Color.Magenta))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview6() {
     ComposeStudyTheme {
-       MainScreen8()
+        MainScreen8()
     }
 }
