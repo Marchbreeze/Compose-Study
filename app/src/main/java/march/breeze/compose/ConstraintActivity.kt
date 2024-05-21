c63ffbc46f30d4bbed49c63421f179ba425d5f24
Sangho Kim <marchbreeze03@gmail.com>
2024-05-21 23:21:13 +0900

    [#30] 버튼 제약 추가

--- app/src/main/java/march/breeze/compose/ConstraintActivity.kt
+++ app/src/main/java/march/breeze/compose/ConstraintActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import march.breeze.compose.ui.theme.ComposeStudyTheme
 
 class ConstraintActivity : ComponentActivity() {
@@ -29,8 +34,24 @@ class ConstraintActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen9() {
+fun MyButton(text: String, modifier: Modifier = Modifier) {
+    Button(
+        onClick = { },
+        modifier = modifier
+    ) {
+        Text(text = text)
+    }
+}
 
+@Composable
+fun MainScreen9() {
+    ConstraintLayout(Modifier.size(200.dp, 200.dp)) {
+        val (button1, button2, button3) = createRefs()
+        MyButton(text = "Button1", Modifier.constrainAs(button1) {
+            top.linkTo(parent.top, margin = 60.dp)
+            start.linkTo(parent.start, margin = 30.dp)
+        })
+    }
 }
 
 @Preview(showBackground = true)
