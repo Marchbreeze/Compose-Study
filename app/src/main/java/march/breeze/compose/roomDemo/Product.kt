4ef7efcbfdde4400b6dbc899559bada71f22f8ce
Sangho Kim <marchbreeze03@gmail.com>
2024-12-27 07:24:14 +0900

    [#44] Room Database 사용 예시 구현

---
+++ app/src/main/java/march/breeze/compose/roomDemo/Product.kt
@@ -0,0 +1,29 @@
+package march.breeze.compose.roomDemo
+
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "products")
+class Product {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "productId")
+    var id: Int = 0
+
+    @ColumnInfo(name = "productName")
+    var productName: String = ""
+    var quantity: Int = 0
+
+    constructor() {}
+
+    constructor(id: Int, productname: String, quantity: Int) {
+        this.productName = productname
+        this.quantity = quantity
+    }
+    constructor(productname: String, quantity: Int) {
+        this.productName = productname
+        this.quantity = quantity
+    }
+}
